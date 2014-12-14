0bf594d347ce6d3cd129538936bd029f8c199022
Adam <quintonish@gmail.com>
2014-12-13 22:46:24 -0700

    Update install.sh
    Added support for 14.10

--- linux/install.sh
+++ linux/install.sh
@@ -1,12 +1,38 @@
+#!/bin/bash
 #This is all you need to run to set up ffeasy
+
+if [ ! -f ffall.sh ]
+then
+   echo "ffeasy files not found! Downloading from github.."
+   wget https://raw.githubusercontent.com/zvakanaka/ffeasy/master/linux/ffall.sh
+   wget https://raw.githubusercontent.com/zvakanaka/ffeasy/master/linux/ffeasy.1.gz
+   wget https://raw.githubusercontent.com/zvakanaka/ffeasy/master/linux/ffeasy.desktop
+   wget https://raw.githubusercontent.com/zvakanaka/ffeasy/master/linux/ffeasy.sh
+   wget https://raw.githubusercontent.com/zvakanaka/ffeasy/master/linux/ffmpeg.xpm
+fi
+#marking runnable files executable
+chmod +x ff*.*s*
 #If ffmpeg not installed, install
 dpkg -s ffmpeg >> ffeasy.log
 if [ $? -eq 1 ]
-     then
-       sudo apt-add-repository ppa:jon-severinsson/ffmpeg
-       sudo apt-get update
-       sudo apt-get install ffmpeg
+then
+   echo "Ffmpeg not found, Attempting to install ffmpeg"
+   lsb_release -a | grep utopic >> /dev/null
+   if [ $? = '1' ]
+   then
+      echo Older Ubuntu, asuming 14.04 
+      sudo apt-add-repository ppa:jon-severinsson/ffmpeg
+      sudo apt-get install ffmpeg
+      sudo apt-get update
+   else
+      echo Ubuntu 14.10 Detected
+      sudo apt-add-repository ppa:samrog131/ppa
+      sudo apt-get update
+      sudo apt-get install ffmpeg-real ffmpeg-set-alternatives
+      sudo ln -sf /opt/ffmpeg/bin/ffmpeg /usr/bin/ffmpeg
+   fi
 fi
+
 echo "additional script for converting, playing, or editing entire folders at once"
 sudo cp -v ffall.sh /usr/bin/ffall
 echo "main script"
